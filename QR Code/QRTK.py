acbfb97823d6727cd18049da944539711cc2468a
Cássio Estevão <116197477+cassioestevao@users.noreply.github.com>
2023-03-06 13:20:36 -0300

    Update QRTK.py

--- QR Code/QRTK.py
+++ QR Code/QRTK.py
@@ -7,23 +7,24 @@ class GeradorQRCode(tk.Frame):
         super().__init__(master)
         self.master = master
         self.master.title("Projeto - Cássio Estevão")
-        self.master.geometry("400x400")
+        self.master.geometry("450x450")
         self.master.configure(bg="purple")
+        self.master.resizable(False,False)
         self.texto_qrcode = tk.StringVar()
         self.criar_widgets()
 
     def criar_widgets(self):
-        self.label = tk.Label(self.master,bg="purple",fg="white",text="Digite o texto que deseja codificar:")
-        self.label.pack()
+        self.label = tk.Label(self.master,bg="purple",fg="white",text="Digite o texto que deseja codificar:", font= "Times 13")
+        self.label.pack(padx=10)
 
-        self.campo_texto = tk.Entry(self.master, textvariable=self.texto_qrcode)
-        self.campo_texto.pack()
+        self.campo_texto = tk.Entry(self.master,width=80,bg="purple",fg="white",font="Times 15",textvariable=self.texto_qrcode)
+        self.campo_texto.pack(padx=10, pady=10)
 
         self.imagem_qrcode = tk.Label(self.master,bg="purple")
         self.imagem_qrcode.pack()
 
-        self.botao = tk.Button(self.master,bg="purple",fg="white",text="Gerar QR Code", command=self.gerar_qrcode)
-        self.botao.pack()
+        self.botao = tk.Button(self.master,bg="purple",fg="white",font="Times 12",text="Gerar QR Code", command=self.gerar_qrcode)
+        self.botao.pack(padx=10, pady=10)
 
     def gerar_qrcode(self):
       
